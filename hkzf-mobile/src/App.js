ac7df0ee834d57248af2ab27abb887c8330f7b58
Jimmy <764350764@qq.com>
2020-06-10 22:09:31 +0800

    react 项目完成

--- hkzf-mobile/src/App.js
+++ hkzf-mobile/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { lazy, Suspense } from 'react';
 
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 // 导入首页和城市选择两个组件(页面)
 import Home from './pages/Home'
 import CityList from './pages/CityList'
-import Map from './pages/Map'
+// import Map from './pages/Map'
 import HouseDetail from './pages/HouseDetail'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
@@ -13,26 +14,29 @@ import Rent from './pages/Rent'
 import RentAdd from './pages/Rent/Add'
 import RentSearch from './pages/Rent/Search'
 import AuthRoute from './components/AuthRoute'
+
+const Map = lazy(() => import('./pages/Map'))
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* 配置路由 */}
-        {/* 默认路由实现重定向 */}
-        <Route exact path="/" render={() => <Redirect to="/home" />}></Route>
+      <Suspense fallback={<div>loading... </div>}>
+        <div className="App">
+          {/* 配置路由 */}
+          {/* 默认路由实现重定向 */}
+          <Route exact path="/" render={() => <Redirect to="/home" />}></Route>
 
-        {/* 父路由 */}
-        <Route path="/home" component={Home}></Route>
-        <Route path="/citylist" component={CityList}></Route>
-        <Route path="/map" component={Map}></Route>
-        <Route path="/detail/:id" component={HouseDetail}></Route>
-        <Route path="/login" component={Login}></Route>
-        <Route path="/profile" component={Profile}></Route>
-        <AuthRoute exact path="/rent" component={Rent}></AuthRoute>
-        <AuthRoute path="/rent/add" component={RentAdd}></AuthRoute>
-        <AuthRoute path="/rent/search" component={RentSearch}></AuthRoute>
-
-      </div>
+          {/* 父路由 */}
+          <Route path="/home" component={Home}></Route>
+          <Route path="/citylist" component={CityList}></Route>
+          <Route path="/map" component={Map}></Route>
+          <Route path="/detail/:id" component={HouseDetail}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/profile" component={Profile}></Route>
+          <AuthRoute exact path="/rent" component={Rent}></AuthRoute>
+          <AuthRoute path="/rent/add" component={RentAdd}></AuthRoute>
+          <AuthRoute path="/rent/search" component={RentSearch}></AuthRoute>
+        </div>
+      </Suspense>
     </Router>
   );
 }
